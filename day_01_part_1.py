5263b4d02c69bd3c0d59b5f172082d6ba5fc36df
lin-ao <58115732+lin-ao@users.noreply.github.com>
2020-12-01 13:46:26 +0100

    Initial Commit

---
+++ day_01_part_1.py
@@ -0,0 +1,31 @@
+def load_input(file_path: str) -> set:
+    with open(file_path, "r") as inp:
+        return [int(line.strip()) for line in inp]
+
+
+def update_list(input_list: list, threshold: int) -> list:
+    return [number for number in input_list if number > threshold]
+
+
+def day_1_part_1(input_list: list, expected_sum: int) -> int:
+    input_half_1 = sorted([number for number in input_list if number <= int(expected_sum / 2)])
+    input_half_2 = sorted(set(input_list).symmetric_difference(input_half_1))
+    for number in input_half_1:
+        if expected_sum - number in input_half_2:
+            return number * (expected_sum - number)
+        else:
+            input_half_2 = update_list(input_half_2, number)
+    return 0
+
+
+def main() -> None:
+    input_list = load_input("day_01_input.txt")
+    answer = day_1_part_1(input_list, 2020)
+    if not answer == 0:
+        print(f"Answer: {answer}")
+    else:
+        print("No answer was found.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ day_01_part_2.py
@@ -0,0 +1,24 @@
+from day_01_part_1 import load_input, day_1_part_1
+
+
+def day_1_part_2(input_list: list, expected_sum: int) -> int:
+    for number in sorted(input_list):
+        temporary_list = input_list[:]
+        temporary_list.remove(number)
+        answer = day_1_part_1(temporary_list, expected_sum - number)
+        if not answer == 0:
+            return answer * number
+    return 0
+
+
+def main() -> None:
+    input_list = load_input("day_01_input.txt")
+    answer = day_1_part_2(input_list, 2020)
+    if not answer == 0:
+        print(f"Answer: {answer}")
+    else:
+        print("No answer was found.")
+
+
+if __name__ == "__main__":
+    main()
