cca4f08ec29efe84e3ca7ba202d7064e88df688c
lin-ao <58115732+lin-ao@users.noreply.github.com>
2020-12-02 15:04:01 +0100

    specified regex to further restrict matches

--- day_02_part_2.py
+++ day_02_part_2.py
@@ -2,7 +2,7 @@ import re
 
 
 def verify_password(line: str) -> bool:
-    parser = re.compile(r"^(\d+)-(\d+)\s(\w):\s(\w+)\s$")
+    parser = re.compile(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)\n$")
     pos1, pos2, letter, password = map(lambda x: int(x) - 1 if x.isdigit() else x, parser.match(line).groups())
     return (password[pos1] == letter) ^ (password[pos2] == letter)
 
