0748ed1eecd5a5dd107d0c7bc3b3cde70833580c
lin-ao <58115732+lin-ao@users.noreply.github.com>
2020-12-07 15:49:08 +0100

    initial commit

---
+++ day_07_part_1.py
@@ -0,0 +1,43 @@
+import re
+from collections import defaultdict
+
+
+def parse_rule(rule: str) -> dict[str, str]:
+    outer_parser = re.compile(r"^([a-z ]+) bags contain ([a-z0-9 ,]+).\n$")
+    outer_bag, inner_bags = outer_parser.match(rule).groups()
+    inner_parser = re.compile(r"(\d+) ([a-z ]+) bag[s]?")
+    rule_dict = {inner_bag[1]: outer_bag for inner_bag in inner_parser.findall(inner_bags)}
+    return rule_dict
+
+
+def load_input(file_path: str) -> dict[str, set]:
+    rules_dict = defaultdict(set)
+    with open(file_path, "r") as file:
+        for line in file:
+            for key, value in parse_rule(line).items():
+                rules_dict[key].add(value)
+    return rules_dict
+
+
+def search_bags(target_colors: set, rules_dict: dict, bags=None) -> int:
+    if bags is None:
+        bags = set()
+    if target_colors:
+        to_search = set()
+        for color in target_colors:
+            to_search.update(rules_dict[color])
+            bags.update(rules_dict[color])
+        return search_bags(to_search, rules_dict, bags)
+    else:
+        return len(bags)
+
+
+def main() -> None:
+    rules_dict = load_input("day_07_input.txt")
+    colors_to_be_searched = {"shiny gold"}
+    answer = search_bags(colors_to_be_searched, rules_dict)
+    print(f"Answer: {answer}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ day_07_part_2.py
@@ -0,0 +1,48 @@
+import re
+from collections import defaultdict
+
+
+def parse_rule(rule: str) -> dict[str, dict[str, int]]:
+    rule_dict = defaultdict(dict)
+    outer_parser = re.compile(r"^([a-z ]+) bags contain ([a-z0-9 ,]+).\n$")
+    outer_bag, inner_bags = outer_parser.match(rule).groups()
+    inner_parser = re.compile(r"(\d+) ([a-z ]+) bag[s]?")
+    for inner_bag in inner_parser.findall(inner_bags):
+        rule_dict[outer_bag].update({inner_bag[1]: int(inner_bag[0])})
+    return rule_dict
+
+
+def load_input(file_path: str) -> dict[str, dict[str, int]]:
+    rules_dict = defaultdict(dict)
+    with open(file_path, "r") as file:
+        for line in file:
+            for key, value in parse_rule(line).items():
+                rules_dict[key] |= value
+    return rules_dict
+
+
+def search_bags(target_colors: dict, rules_dict: dict, bags=0) -> int:
+    if target_colors:
+        to_search = defaultdict(lambda: 0)
+        contains_bags = set(target_colors.keys()).intersection(rules_dict.keys())
+        do_not_contain_bags = target_colors.keys() - contains_bags
+        for color in contains_bags:
+            bags += target_colors[color]
+            for item in rules_dict[color].items():
+                to_search[item[0]] += target_colors[color] * item[1]
+        for color in do_not_contain_bags:
+            bags += target_colors[color]
+        return search_bags(to_search, rules_dict, bags)
+    else:
+        return bags - 1
+
+
+def main() -> None:
+    rules_dict = load_input("day_07_input.txt")
+    colors_to_be_searched = {"shiny gold": 1}
+    answer = search_bags(colors_to_be_searched, rules_dict)
+    print(f"Answer: {answer}")
+
+
+if __name__ == "__main__":
+    main()
